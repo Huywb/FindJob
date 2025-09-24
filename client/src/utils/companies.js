49dd75fd14564694e11d21ff0c28f646add50497
huywb <pthhuy1306@gmail.com>
2025-09-24 15:06:12 +0700

    Companies data

---
+++ client/src/utils/companies.js
@@ -0,0 +1,42 @@
+export const companies = [
+  {
+    id:'1',
+    name: "Google", 
+    logo: "../../public/companies/google.webp",
+  },
+  {
+    id:'2',
+    name: "Microsoft", 
+    logo: "../../public/companies/microsoft.webp",
+  },
+  {
+    id:'3',
+    name: "Amazon", 
+    logo: "../../public/companies/amazon.svg",
+  },
+  {
+    id:'4',
+    name: "Meta", 
+    logo: "../../public/companies/meta.svg",
+  },
+  {
+    id:'5',
+    name: "Netflix", 
+    logo: "../../public/companies/netflix.png",
+  },
+  {
+    id:'6',
+    name: "Atlassian", 
+    logo: "../../public/companies/atlassian.svg",
+  },
+  {
+    id:'8',
+    name: "IBM", 
+    logo: "../../public/companies/ibm.svg",
+  },
+  {
+    id:'9',
+    name: "Uber", 
+    logo: "../../public/companies/uber.svg",
+  }
+]
