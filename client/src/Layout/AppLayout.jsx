2955451cea6877dbc79bc8ce24b4fb432c6b7a62
huywb <pthhuy1306@gmail.com>
2025-09-24 15:06:42 +0700

    Update Layout

--- client/src/Layout/AppLayout.jsx
+++ client/src/Layout/AppLayout.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Button } from '@/components/ui/button'
+import Header from '@/components/Header'
 
 const AppLayout = () => {
   return (
-    <div>
+    <div >
       <div className='grid-background '></div>
-      <main className=' min-h-screen container'>
-          <Outlet></Outlet>
+      <main className=' min-h-screen container px-8'>
+        <Header></Header>
+        <Outlet></Outlet>
       </main>
+      <div className='p-10 text-center'></div>
     </div>
   )
 }
